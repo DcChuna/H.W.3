1b0bd7fe49330bb30664dcf37a64793a8768c723
Nazar <nazarhuzner@gmail.com>
2022-11-05 10:42:07 +0200

    середнє арефметичене дз 3

--- main.py
+++ main.py
@@ -9,17 +9,3 @@ try:
     print((nn+nn1+nn2+nn3+nn4)/5)
 except (ImportError,TypeError,NameError,ValueError):
     print('Only number 1|not text')
-infologging=input("Веддіть свій логін: ")
-password=input("Веддіть свій пароль: ")
-logging.basicConfig(
-    level=logging.INFO,
-    filename = 'logs.log',
-    filemode='a',
-    format=f'[%(asctime)s] %(message)s'
-)
-logging.info(infologging)
-logging.info(password)
-logging.debug("antivirus!")
-logging.warning("WARNING!")
-logging.error("Antivirus activate!")
-logging.critical("All ok virus not find")
